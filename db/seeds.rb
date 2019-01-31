215ab4eadc0cb60fd7f1d5e74d30ae5564ddfee5
Bozotte <armelle.peuvion.weiss@gmail.com>
2019-01-31 17:47:06 +0100

    ajouts

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,22 @@
 require 'faker'
 
-10.times do
-	city = City.create!(name: Faker::Nation.capital_city)
+12.times do
+	city = City.create!(name: Faker::Address.city)
 end
 
-30.times do
+15.times do
 	dogsitter = Dogsitter.new(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
 	dogsitter.city = City.all.sample
 	dogsitter.save
 end
 
-30.times do
-	dog = Dog.new(name: Faker::Pokemon.name)
+20.times do
+	dog = Dog.new(name: Faker::Artist.name)
 	dog.city = City.all.sample
 	dog.save
 end
 
-50.times do
+30.times do
 	stroll = Stroll.new(date: Faker::Time.backward(14, :all))
 	stroll.dogsitter = Dogsitter.all.sample
 	stroll.dog = Dog.all.sample
